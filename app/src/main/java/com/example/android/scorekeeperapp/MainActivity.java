b7b5a481d5f5b730c1cea243faa072c37c03d9cb
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-05 19:19:57 +0000

    add function team A foul

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -7,9 +7,13 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-//    football with score and fouls
+    //    basketball game with score and fouls
+//    global variables for keeping score
     int team_a_score = 0;
     int team_b_score = 0;
+    //    global variables for keeping score
+    int team_a_fouls = 0;
+    int team_b_fouls = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         displayForTeamA(team_a_score);
         displayForTeamB(team_b_score);
+        displayFoulsTeamA(team_a_fouls);
     }
 
     //team A score
     public void displayForTeamA(int score) {
         TextView scoreViewA = findViewById(R.id.team_a_score);
         scoreViewA.setText(String.valueOf(score));
+    }
+
+    //team A fouls
+    public void displayFoulsTeamA(int foul) {
+        TextView foulViewA = findViewById(R.id.team_a_fouls);
+        foulViewA.setText(String.valueOf(foul));
     }
 
     public void increment3PointsA(View view) {
@@ -38,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
     public void increment1PointA(View view) {
         team_a_score += 1;
         displayForTeamA(team_a_score);
+    }
+
+    public void addFoulsTeamA(View view) {
+        team_a_fouls += 1;
+        displayForTeamA(team_a_fouls);
     }
 
     //    team B score
